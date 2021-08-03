e5e5f4f6fd3931835309a3d5ab72cd82f91a83b3
Cano XL <gcanoxl@gmail.com>
2021-08-03 14:42:35 +0800

    add exec-path-from-shell

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,3 +1,7 @@
+(straight-use-package 'exec-path-from-shell)
+(when (memq window-system '(mac ns x))
+  (exec-path-from-shell-initialize))
+
 (require 'init-evil)
 (require 'init-completion)
 (require 'init-lsp)
