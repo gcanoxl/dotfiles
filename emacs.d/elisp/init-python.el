0c9e7438161dbc66a0ac7d7ba202db660bbe18cc
Cano XL <gcanoxl@gmail.com>
2021-08-05 20:47:30 +0800

    delete used .el files & add emacslisp support

--- emacs.d/elisp/init-python.el
+++
@@ -1,6 +0,0 @@
-(straight-use-package 'lsp-pyright)
-
-;; ==================== Keybindings ====================
-(evil-define-key 'normal 'python-mode-map (kbd "<localleader>r") 'recompile)
-
-(provide 'init-python)
