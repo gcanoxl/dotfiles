25ed94e5551e5bf2a4b953df6aefd0316ac3f7ca
Cano XL <gcanoxl@gmail.com>
2021-10-06 19:34:04 +0800

    add org-capture keybinding & setq truncate-lines to q

--- emacs.d/elisp/init-org.el
+++ emacs.d/elisp/init-org.el
@@ -1,13 +1,17 @@
 ;; ==================== Org-Capture ====================
 (setq org-capture-templates
-	  '(("j" "Journal" entry (file+datetree "~/Dropbox/Cano's Orgs/test-journal.org")
+	  '(("j" "Journal" entry (file+datetree "~/Dropbox/Org/Journal.org.gpg")
 		 "* %U - %^{heading}\n  %?")))
+
+(evil-define-key 'normal 'global (kbd "<leader>oc") 'org-capture)
+
+(setq truncate-lines t)
 
 ;; ==================== EPA ====================
 (epa-file-enable)
 (setq epa-pinentry-mode 'loopback)
 
-(add-hook 'kill-emacs-hook (defun personal--kill-gpg-agent ()
+(add-hook 'kill-emacs-hook (defun gcanoxl/personal--kill-gpg-agent ()
                              (shell-command "pkill gpg-agent")))
 
 (provide 'init-org)
