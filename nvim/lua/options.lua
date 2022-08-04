2119a42eab3a24766e056b99e6daa80fb76f2dc6
Cano XL <gcanoxl@gmail.com>
2022-08-04 21:10:25 +0800

    add lua/options.lua

---
+++ nvim/lua/options.lua
@@ -0,0 +1,19 @@
+local options = {
+	clipboard = 'unnamedplus',
+	termguicolors = true,
+	mouse = 'a',
+	fileencoding = 'utf-8',
+	ignorecase = true,
+	autoindent = true,
+	tabstop = 2,
+	shiftwidth = 2,
+	number = true,
+	relativenumber = false,
+	wrap = true,
+	cursorline = true,
+	cursorcolumn = true
+}
+
+for k, v in pairs(options) do
+    vim.opt[k] = v
+end
