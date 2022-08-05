984ed91e72789e6c3a4e53f4b0cd53b3de7967eb
Cano XL <gcanoxl@gmail.com>
2022-08-05 10:45:20 +0800

    change colorscheme to tokyonight

--- nvim/lua/GUI.lua
+++ nvim/lua/GUI.lua
@@ -2,12 +2,16 @@ local use = require'plugins'
 
 -- status line
 use {
-  'nvim-lualine/lualine.nvim',
-  requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+	'nvim-lualine/lualine.nvim',
+	requires = { 'kyazdani42/nvim-web-devicons', opt = true }
 }
 
-require('lualine').setup()
 
--- colorscheme
+require('lualine').setup({})
+
+-- colorschemes
 use 'dr4vs/neon.nvim'
-vim.cmd[[colorscheme neon]]
+use 'folke/tokyonight.nvim'
+
+vim.g.tokyonight_style = "storm"
+vim.cmd[[colorscheme tokyonight]]
