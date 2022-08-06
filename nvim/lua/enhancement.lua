b9a313bad21d9ff27f96b8a143eff8a1debdd21e
Cano XL <gcanoxl@gmail.com>
2022-08-06 19:47:33 +0800

    delete completion.lua & add telescope oldfiles keymap

--- nvim/lua/enhancement.lua
+++ nvim/lua/enhancement.lua
@@ -1,10 +1,30 @@
+-- tree-sitter
+require'plugins' {
+	'nvim-treesitter/nvim-treesitter',
+	run = ':TSUpdate'
+}
+
+-- telescope
+require'plugins' {
+	'nvim-telescope/telescope.nvim', tag = '0.1.0',
+	requires = { {'nvim-lua/plenary.nvim'} }
+}
+
+-- keymaps
+local nomap = require'keymaps'
+nomap.nc('<C-c>', 'Telescope buffers')
+nomap.lnc('<leader>', 'Telescope commands')
+nomap.lnc('ff', 'Telescope find_files')
+nomap.lnc('fr', 'Telescope oldfiles')
+nomap.lnc('ss', 'Telescope live_grep')
+nomap.lnc('hh', 'Telescope help_tags')
+
 -- todolist
 require'plugins' {
 	"folke/todo-comments.nvim",
 	requires = "nvim-lua/plenary.nvim",
 }
 require("todo-comments").setup {}
-local nomap = require'keymaps'
 nomap.lnc('ts', 'TodoTelescope')
 nomap.lnc('tt', 'TodoQuickFix')
 
