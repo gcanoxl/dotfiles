e1f3899f270005c0f4388319828ff35f8653e2ed
Cano XL <gcanoxl@gmail.com>
2022-08-07 15:52:32 +0800

    implemented plugin installation

--- nvim/lua/plugin.lua
+++ nvim/lua/plugin.lua
@@ -1,37 +1,32 @@
 local geem_plugins = {
-
 	{
-		['"folke/which-key.nvim"'] = {
-			config = function()
-				require("which-key").setup {
-					-- your configuration comes here
-					-- or leave it empty to use the default settings
-					-- refer to the configuration section below
-				}
-			end
-		}
+		'"folke/which-key.nvim"',
+		config = function()
+			require("which-key").setup {
+				-- your configuration comes here
+				-- or leave it empty to use the default settings
+				-- refer to the configuration section below
+			}
+		end
 	},
 
 	{
-		['goolord/alpha-nvim'] = {
-			config = function()
-				require 'alpha'.setup(require 'alpha.themes.dashboard'.config)
-			end
-		}
+		'goolord/alpha-nvim',
+		config = function()
+			require 'alpha'.setup(require 'alpha.themes.dashboard'.config)
+		end
 	},
 
 	{
-		["folke/trouble.nvim"] = {
-
-			requires = "kyazdani42/nvim-web-devicons",
-			config = function()
-				require("trouble").setup {
-					-- your configuration comes here
-					-- or leave it empty to use the default settings
-					-- refer to the configuration section below
-				}
-			end
-		}
+		"folke/trouble.nvim",
+		requires = "kyazdani42/nvim-web-devicons",
+		config = function()
+			require("trouble").setup {
+				-- your configuration comes here
+				-- or leave it empty to use the default settings
+				-- refer to the configuration section below
+			}
+		end
 
 	}
 }
@@ -42,10 +37,8 @@ local status_ok, packer = pcall(require, 'packer')
 if status_ok then
 	packer.startup {
 		function(use)
-			for _, plugin_list in ipairs(geem_plugins) do
-				for name, options in pairs(plugin_list) do
-					use(name)
-				end
+			for _, plugin in ipairs(geem_plugins) do
+				use(plugin)
 			end
 		end
 	}
