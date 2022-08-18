c84957e2f7555e21249017caf7141873f082e4af
Cano XL <gcanoxl@gmail.com>
2022-08-19 05:41:41 +0800

    added some treesitter's server

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,9 @@
-local status_ok, treesitter_configs = pcall(require, 'nvim_treesitter.configs')
+local status_ok, treesitter_configs = pcall(require, 'nvim-treesitter.configs')
 if not status_ok then return end
 
 treesitter_configs.setup {
-	ensure_installed = { "lua", "vim" },
+	ensure_installed = { "lua", "vim", "c", "python", "go", "dart", "javascript" },
+	highlight = {
+		enable = true,
+	},
 }
