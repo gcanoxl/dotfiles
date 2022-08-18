6b33e198c299651109b47460068e701d424c5a82
Cano XL <gcanoxl@gmail.com>
2022-08-18 14:04:12 +0800

    delete utils.dev

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,13 +1,6 @@
 require 'options'
 require 'utils'
 
-
-
--- disable this line if you are not developing the config
-require 'utils.dev'
-
-
-
 --
 require 'plugins'
 require 'keymaps'
@@ -15,7 +8,7 @@ require 'keymaps'
 --------
 
 
--- require 'GUI'
+--require 'GUI'
 -- require 'files'
 -- require 'editing'
 -- require 'lsp'
