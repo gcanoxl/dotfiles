ae8d6005c3a8ceba1858280571e6936498fb3d14
Cano XL <gcanoxl@gmail.com>
2022-08-18 13:45:26 +0800

    working on implementing mapping

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -18,16 +18,22 @@ local function map_i(key, cmd, desc)
 	map_table.i[key] = { cmd = cmd, desc = desc }
 end
 
---keymaps
+-- leader
+vim.g.maplader = ' '
+print(vim.g.mapleader)
 
+--keymaps
 map_i('jk', '<ESC>', 'Quit Insert Mode')
 map_n_command_leader('fw', 'write', 'Save File')
-map_n_command('<D-s>', 'write', 'Save File')
 map_n_command_leader('qq', 'quit', "Quit")
 
 -- auto mapping
+-- FIX: implement this functionality
 for mode, maps in pairs(map_table) do
 	for keymap, options in pairs(maps) do
-		-- TODO: implement mapping
+		-- TODO: Delete this line
+		print(keymap .. ' -> ' .. options.cmd)
+		vim.api.nvim_set_keymap(mode, keymap, options.cmd,
+			{ noremap = true, silent = true })
 	end
 end
