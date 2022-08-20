d668615dfb5e369e7309802d22e480c8e347407c
Cano XL <gcanoxl@gmail.com>
2022-08-20 08:27:48 +0800

    fixed lsp config, even though I don't know how...

--- nvim/lua/plugins/lsp/init.lua
+++ nvim/lua/plugins/lsp/init.lua
@@ -1,34 +1,32 @@
 local status_ok, lspconfig = pcall(require, 'lspconfig')
 if not status_ok then return end
 
-require 'lspconfig'.pyright.setup {}
-
 -- enable some language servers with the additional completion capabilities offered by nvim-cmp
--- local servers = { 'sumneko_lua', 'vimls' }
---
--- local capabilities = require 'plugins.lsp.handlers'
---
--- for _, lsp in ipairs(servers) do
--- 	lspconfig[lsp].setup {
--- 		capabilities = capabilities,
--- 	}
--- end
---
--- -- auto format on save
--- vim.cmd [[
--- augroup formatOnSave
---     autocmd!
---     autocmd BufWritePre *.lua :lua vim.lsp.buf.formatting_sync()
--- augroup END
--- ]]
---
--- -- lua remove annoying warnings
--- lspconfig.sumneko_lua.setup {
--- 	settings = {
--- 		Lua = {
--- 			diagnostics = {
--- 				globals = { 'vim' }
--- 			}
--- 		}
--- 	}
--- }
+local servers = { 'sumneko_lua', 'vimls', 'pyright'}
+
+local capabilities = require 'plugins.lsp.handlers'
+
+for _, lsp in ipairs(servers) do
+	lspconfig[lsp].setup {
+		capabilities = capabilities,
+	}
+end
+
+-- auto format on save
+vim.cmd [[
+augroup formatOnSave
+    autocmd!
+    autocmd BufWritePre *.lua :lua vim.lsp.buf.formatting_sync()
+augroup END
+]]
+
+-- lua remove annoying warnings
+lspconfig.sumneko_lua.setup {
+		settings = {
+				Lua = {
+						diagnostics = {
+								globals = { 'vim' }
+							}
+						}
+					}
+				}
