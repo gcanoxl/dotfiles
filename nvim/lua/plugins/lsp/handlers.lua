3a2fb1f08a160525ee8589c561100b6016c6e570
Cano XL <gcanoxl@gmail.com>
2022-09-01 06:01:27 +0800

    add code action keymap

--- nvim/lua/plugins/lsp/handlers.lua
+++ nvim/lua/plugins/lsp/handlers.lua
@@ -3,34 +3,6 @@ if not cmp_lsp_status_ok then
 	return
 end
 
--- handlers
-vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
-vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
-
--- define icons
-local signs = {
-	{ name = "DiagnosticSignError", text = "" },
-	{ name = "DiagnosticSignWarn", text = "" },
-	{ name = "DiagnosticSignHint", text = "" },
-	{ name = "DiagnosticSignInfo", text = "" },
-}
-for _, sign in ipairs(signs) do
-	vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
-end
-
-vim.diagnostic.config({
-	virtual_text = true,
-	signs = { active = signs },
-	update_in_insert = true,
-	float = {
-		border = "rounded",
-	}
-})
-
--- capabilities
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = cmp_lsp.update_capabilities(capabilities)
-
 -- on_attach function
 local on_attach = function(_, bufnr)
 	local bufopts = { noremap = true, silent = true, buffer = bufnr }
@@ -38,6 +10,15 @@ local on_attach = function(_, bufnr)
 	vim.keymap.set('n', '<localleader>dp', vim.diagnostic.goto_prev, bufopts)
 	vim.keymap.set('n', '<localleader>dn', vim.diagnostic.goto_next, bufopts)
 	vim.keymap.set('n', '<C-k>', vim.lsp.buf.hover, bufopts)
+	vim.keymap.set('n', '<localleader>ca', vim.lsp.buf.code_action, bufopts)
 end
+
+-- handlers
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
+
+-- capabilities
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = cmp_lsp.update_capabilities(capabilities)
 
 return on_attach, capabilities
--- nvim/lua/plugins/lsp/init.lua
+++ nvim/lua/plugins/lsp/init.lua
@@ -1,6 +1,26 @@
 local status_ok, lspconfig = pcall(require, 'lspconfig')
 if not status_ok then return end
 
+-- define icons
+local signs = {
+	{ name = "DiagnosticSignError", text = "" },
+	{ name = "DiagnosticSignWarn", text = "" },
+	{ name = "DiagnosticSignHint", text = "" },
+	{ name = "DiagnosticSignInfo", text = "" },
+}
+for _, sign in ipairs(signs) do
+	vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
+end
+
+vim.diagnostic.config({
+	virtual_text = true,
+	signs = { active = signs },
+	update_in_insert = true,
+	float = {
+		border = "rounded",
+	}
+})
+
 -- enable some language servers with the additional completion capabilities offered by nvim-cmp
 local servers = { 'sumneko_lua', 'vimls', 'pyright', 'gopls' }
 
