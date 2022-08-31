9bf23c79016b8cd63d7f7a1f990b0555f2f11acd
Cano XL <gcanoxl@gmail.com>
2022-08-31 23:26:58 +0800

    add hammerspoon's library to sumneko settings

--- nvim/lua/plugins/lsp/server-settings/sumneko_lua.lua
+++ nvim/lua/plugins/lsp/server-settings/sumneko_lua.lua
@@ -2,11 +2,12 @@ return {
 	settings = {
 		Lua = {
 			diagnostics = {
-				globals = { 'vim' }
+				globals = { 'vim', 'hs' }
 			},
 			workspace = {
 				library = {
 					[vim.fn.expand "$VIMRUNTIME/lua"] = true,
+					["/Applications/Hammerspoon.app/Contents/Resources/extensions"] = true
 				},
 			},
 		}
