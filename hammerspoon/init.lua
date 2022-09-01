723ddb303ea302083990c463a53d5ededcbff8d9
Cano XL <gcanoxl@gmail.com>
2022-09-01 18:24:21 +0800

    add toggle fullscreen for all windows

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,5 +1,11 @@
--- Configuration Reloading
+-- configuration reloading
 hs.loadSpoon("ReloadConfiguration")
 spoon.ReloadConfiguration:start()
 
--- TODO: toggle fullscreen
+-- toggle fullscreene
+hs.hotkey.bind({ 'cmd' }, 'return', function()
+	local window = hs.window.focusedWindow()
+	window:setFullscreen(not window:isFullscreen())
+end)
+
+-- TODO: auto subscribling
