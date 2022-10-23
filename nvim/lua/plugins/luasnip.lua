23f2378f7cc18e3db2c1ce68b173cacc179f2a2d
Cano XL <gcanoxl@gmail.com>
2022-10-24 04:10:30 +0800

    load snippets from lua

--- nvim/lua/plugins/luasnip.lua
+++ nvim/lua/plugins/luasnip.lua
@@ -1,5 +1,5 @@
-local avali, ls = pcall(require, 'luasnip')
+local avali, _ = pcall(require, 'luasnip')
 local lua_loader_avail, lua_loader = pcall(require, "luasnip/loaders/from_lua")
 if not (avali and lua_loader_avail) then return end
 
-lua_loader.lazy_load('./snippets')
+lua_loader.lazy_load({ paths = "./snippets" })
