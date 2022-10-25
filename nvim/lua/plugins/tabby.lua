27c37ce38566eabded0903acf1bfae693e1c7539
Cano XL <gcanoxl@gmail.com>
2022-10-26 04:28:49 +0800

    don't display tabline automatically

--- nvim/lua/plugins/tabby.lua
+++ nvim/lua/plugins/tabby.lua
@@ -2,6 +2,6 @@ local status_ok, tabby = pcall(require, 'tabby.tabline')
 if not status_ok then return end
 
 -- always display tabline
-vim.o.showtabline = 2
+-- vim.o.showtabline = 2
 
 tabby.use_preset('tab_with_top_win', {})
