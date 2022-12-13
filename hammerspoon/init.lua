35a8efcc0b6c8868931f784ba0254926ca805c41
Cano XL <cano@Canos-MacBook-Air.local>
2022-12-13 13:23:04 +0800

    add neovide layout for flutter development

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -7,4 +7,12 @@ hs.hotkey.bind({ 'cmd', 'ctrl' }, 'return', function()
 	local window = hs.window.focusedWindow()
 	window:toggleFullScreen()
 end)
--- TODO: auto subscribling
+
+-- window layout for developing
+local laptopScreen = "developing"
+local windowLayout = {
+	{ "Neovide", nil, laptopScreen, { x = 0, y = 0, w = 0.73, h = 1 }, nil, nil },
+}
+hs.hotkey.bind({ 'cmd', 'ctrl' }, 'd', function()
+	hs.layout.apply(windowLayout)
+end)
