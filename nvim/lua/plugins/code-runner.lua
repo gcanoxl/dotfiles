07cd0ebd1ec2ca0750e5d486508c851c60617aed
Cano XL <gcanoxl@gmail.com>
2023-03-29 09:57:15 +0800

    add c support and code_runner

--- nvim/lua/plugins/code-runner.lua
+++ nvim/lua/plugins/code-runner.lua
@@ -1,4 +1,14 @@
 local code_runner_avail, code_runner = pcall(require, 'code_runner');
 if not code_runner_avail then return end
 
-code_runner.setup({})
+code_runner.setup({
+	focus = false,
+	term = {
+		position = "belowright",
+		size = 8,
+	},
+	filetype = {
+		python = "python3 -u",
+		c = "gcc $fileName && ./a.out"
+	}
+})
