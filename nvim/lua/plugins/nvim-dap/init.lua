a4846804cd8c9ef27625e408c418b56e72e13e86
Cano XL <gcanoxl@gmail.com>
2023-04-08 03:33:51 +0800

    change dap-ui's layout

--- nvim/lua/plugins/nvim-dap/init.lua
+++ nvim/lua/plugins/nvim-dap/init.lua
@@ -12,4 +12,27 @@ local dapui_ok, dapui = pcall(require, 'dapui')
 if not dapui_ok then
 	return
 end
-dapui.setup()
+dapui.setup({
+	layouts = {
+		{
+			-- You can change the order of elements in the sidebar
+			elements = {
+				-- Provide IDs as strings or tables with "id" and "size" keys
+				{ id = "scopes",      size = 2, },
+				{ id = "breakpoints", size = 0.25 },
+				{ id = "stacks",      size = 0.25 },
+				{ id = "watches",     size = 0.25 },
+			},
+			size = 24,
+			position = "left", -- Can be "left" or "right"
+		},
+		{
+			elements = {
+				{ id = "console", size = 0.4 },
+				{ id = "repl",    size = 0.6 },
+			},
+			size = 8,
+			position = "bottom", -- Can be "bottom" or "top"
+		},
+	},
+})
