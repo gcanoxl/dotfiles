936bd1f742568db066531ac39851e92740546dab
Cano XL <gcanoxl@gmail.com>
2023-05-22 15:09:25 +0800

    move catppuccin.lua

--- nvim/lua/new-plugins/colorschemes.lua
+++ nvim/lua/new-plugins/colorschemes.lua
@@ -4,6 +4,21 @@ return {
 		"catppuccin/nvim",
 		name = "catppuccin",
 		lazy = true,
+		config = function()
+			require('catppuccin').setup({
+				flavour = "frappe",
+				integrations = {
+					neogit = false,
+					hop = true,
+					telescope = true,
+					neotree = {
+						enabled = true,
+						show_root = true,
+						transparent_panel = false,
+					}
+				}
+			})
+		end
 	},
 
 	-- nightfox
