95f6f67908862cb0c3d3ade3527197c2126686a2
Cano XL <gcanoxl@gmail.com>
2023-05-22 14:45:48 +0800

    move colorschemes.lua

---
+++ nvim/lua/new-plugins/colorschemes.lua
@@ -0,0 +1,21 @@
+return {
+	-- catppuccin
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		lazy = true,
+	},
+
+	-- nightfox
+	{
+		"EdenEast/nightfox.nvim",
+		lazy = true,
+
+	},
+
+	-- tokyonight
+	{
+		'folke/tokyonight.nvim',
+		lazy = true,
+	},
+}
