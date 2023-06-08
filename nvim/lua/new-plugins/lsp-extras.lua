99b788faa2e6156e010b35b5a983e9b882ee1666
Cano XL <gcanoxl@gmail.com>
2023-06-08 20:30:01 +0800

    use null-ls.nvim

---
+++ nvim/lua/new-plugins/lsp-extras.lua
@@ -0,0 +1,19 @@
+return {
+	'jose-elias-alvarez/null-ls.nvim',
+	config = function()
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = {
+				-- null_ls.builtins.formatting.stylua,
+				-- null_ls.builtins.diagnostics.eslint,
+				-- null_ls.builtins.completion.spell,
+				null_ls.builtins.formatting.isort,
+				null_ls.builtins.formatting.black.with({
+					{
+						extra_args = { "--line-length=120" }
+					}
+				})
+			},
+		})
+	end
+}
