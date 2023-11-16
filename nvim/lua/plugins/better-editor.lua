3bc7c98042cc5738f19b04648855b496b93cc454
Cano XL <gcanoxl@gmail.com>
2023-11-16 18:48:12 +0800

    style

--- nvim/lua/plugins/better-editor.lua
+++ nvim/lua/plugins/better-editor.lua
@@ -14,7 +14,7 @@ return {
 			vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
 			vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
 			require('ufo').setup({
-				provider_selector = function(bufnr, filetype, buftype)
+				provider_selector = function(_, _, _)
 					return { 'treesitter', 'indent' }
 				end
 			})
--- nvim/lua/plugins/terminal.lua
+++ nvim/lua/plugins/terminal.lua
@@ -9,14 +9,5 @@ return {
 				winblend = vim.g.preference.winblend
 			}
 		})
-		local Terminal  = require('toggleterm.terminal').Terminal
-		local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
-
-		function Lazygit_toggle()
-			lazygit:toggle()
-		end
-
-		vim.api.nvim_set_keymap("n", "<leader>G", "<cmd>lua Lazygit_toggle()<CR>", {noremap = true, silent = true})
 	end
 }
-
