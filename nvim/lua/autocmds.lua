a318d294cdc3451e8a0e02bc073b3b3a8489f4db
Cano XL <gcanoxl@gmail.com>
2023-11-16 17:54:10 +0800

    add autocmd for auto fixing dart files

---
+++ nvim/lua/autocmds.lua
@@ -0,0 +1,11 @@
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = "*.dart",
+	callback = function()
+		vim.lsp.buf.code_action({
+			apply = true,
+			filter = function(action)
+				return action.title == "Fix All"
+			end
+		})
+	end
+})
