ecb53e5b9d603a1239fb39c5b6d73077105490ec
Cano XL <gcanoxl@gmail.com>
2023-12-31 05:36:59 +0800

    delete jk keymaps for terminal mode

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -19,7 +19,6 @@ vim.keymap.set("n", "<C-f>", geem.cmd("Telescope find_files"))
 
 -- Terminal
 vim.keymap.set("n", "!", geem.cmd("ToggleTerm"))
-vim.keymap.set("t", "jk", "<C-\\><C-n>")
 
 -- Emoji
 vim.keymap.set("i", "<C-e>", geem.cmd("Telescope emoji"))
--- nvim/lua/plugins/terminal.lua
+++ nvim/lua/plugins/terminal.lua
@@ -1,6 +1,5 @@
 return {
 	"akinsho/toggleterm.nvim",
-	-- version = 'v2.*',
 	config = function()
 		require("toggleterm").setup({
 			direction = 'tab',
