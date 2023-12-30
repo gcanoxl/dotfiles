46433d93d6393b76d9eb6e2ee582276f2b3df1f1
Cano XL <gcanoxl@gmail.com>
2023-12-31 03:18:39 +0800

    add auto cd autocmds

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -1,12 +1,28 @@
+local usergroup = vim.api.nvim_create_augroup("usergroup", { clear = true })
+
 -- auto perform `Fix All` code action on save
 vim.api.nvim_create_autocmd("BufWritePre", {
 	pattern = "*.dart",
+	group = "usergroup",
 	callback = function()
 		vim.lsp.buf.code_action({
 			apply = true,
 			filter = function(action)
 				return action.title == "Fix All"
-			end
+			end,
 		})
-	end
+	end,
+})
+
+-- Auto Cd to current dir when opening a single file in command line
+vim.api.nvim_create_autocmd("VimEnter", {
+	group = "usergroup",
+	callback = function()
+		if vim.fn.argc() == 1 then
+			local filename = vim.fn.expand("%:p")
+			if vim.fn.filereadable(filename) == 1 then
+				vim.cmd("cd " .. vim.fn.fnamemodify(filename, ":h"))
+			end
+		end
+	end,
 })
