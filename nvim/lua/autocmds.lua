9b72337ed8070ff06e07718431fc536696ede576
Cano XL <gcanoxl@gmail.com>
2023-12-31 03:31:22 +0800

    add auto close unmodified bufs

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -3,7 +3,7 @@ local usergroup = vim.api.nvim_create_augroup("usergroup", { clear = true })
 -- auto perform `Fix All` code action on save
 vim.api.nvim_create_autocmd("BufWritePre", {
 	pattern = "*.dart",
-	group = "usergroup",
+	group = usergroup,
 	callback = function()
 		vim.lsp.buf.code_action({
 			apply = true,
@@ -16,13 +16,23 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 
 -- Auto Cd to current dir when opening a single file in command line
 vim.api.nvim_create_autocmd("VimEnter", {
-	group = "usergroup",
+	group = usergroup,
 	callback = function()
 		if vim.fn.argc() == 1 then
 			local filename = vim.fn.expand("%:p")
 			if vim.fn.filereadable(filename) == 1 then
 				vim.cmd("cd " .. vim.fn.fnamemodify(filename, ":h"))
 			end
+		end
+	end,
+})
+
+-- Close buf when leave if it's not modified and empty
+vim.api.nvim_create_autocmd("BufLeave", {
+	group = usergroup,
+	callback = function()
+		if vim.bo.modified == false and vim.bo.buftype == "" and vim.fn.line("$") == 1 then
+			vim.cmd("bd")
 		end
 	end,
 })
