94a79804bc5df5f23882f079a56f296a693c68de
Cano XL <gcanoxl@gmail.com>
2024-01-25 04:25:35 +0800

    nvim: add configrations for dap

--- nvim/lua/configs/dap/init.lua
+++ nvim/lua/configs/dap/init.lua
@@ -1,23 +1,11 @@
-vim.api.nvim_set_hl(0, "DapBreakpoint", { ctermbg = 0, fg = "#993939", bg = "#31353f" })
-vim.api.nvim_set_hl(0, "DapLogPoint", { ctermbg = 0, fg = "#61afef", bg = "#31353f" })
-vim.api.nvim_set_hl(0, "DapStopped", { ctermbg = 0, fg = "#98c379", bg = "#31353f" })
+require("configs.dap.highlight")
 
-vim.fn.sign_define("DapBreakpoint", {
-	text = "",
-	texthl = "DapBreakpoint",
-	linehl = "DapBreakpoint",
-	numhl = "DapBreakpoint",
-})
-vim.fn.sign_define(
-	"DapBreakpointCondition",
-	{ text = "ﳁ", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
-)
-vim.fn.sign_define(
-	"DapBreakpointRejected",
-	{ text = "", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
-)
-vim.fn.sign_define(
-	"DapLogPoint",
-	{ text = "", texthl = "DapLogPoint", linehl = "DapLogPoint", numhl = "DapLogPoint" }
-)
-vim.fn.sign_define("DapStopped", { text = "", texthl = "DapStopped", linehl = "DapStopped", numhl = "DapStopped" })
+-- load all files in the cfgs directory
+local filelist = vim.fn.readdir(vim.fn.expand("~/.config/nvim/lua/configs/dap/cfgs"))
+for _, fname in ipairs(filelist) do
+	local path = "configs.dap.cfgs." .. fname:gsub(".lua", "")
+	local ok, _ = pcall(require, path)
+	if not ok then
+		vim.notify("Error loading: " .. path, vim.log.levels.ERROR)
+	end
+end
