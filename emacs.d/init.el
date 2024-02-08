7f88cd26d7ea0204e6ec805ba482441368c62ac6
Cano XL <gcanoxl@gmail.com>
2024-02-08 18:58:16 +0800

    emacs: update init.el

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,45 +1,43 @@
-;; basic
+;; basic settings
 (tool-bar-mode -1)
-(global-display-line-numbers-mode)
-(setq cursor-type 'bar)
 (scroll-bar-mode -1)
-;;(set-face-attribute 'default nil :height 160)
-(global-hl-line-mode 1)
+(global-display-line-numbers-mode t)
 
 ;; package installation
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
- ;;(when (not package-archive-contents)
-   ;;(package-refresh-contents))
-(package-initialize)
+
+;; theme
+(package-install 'zenburn-theme)
+(package-install 'spacemacs-theme)
+(load-theme 'spacemacs-dark t)
+
+;; font
+(when (member "Courier New" (font-family-list))
+  (set-frame-font "Courier New-20:bold"))
+
+;; Evil
+(package-install 'evil)
+(package-install 'evil-escape)
+(setq evil-want-C-u-scroll t)
+(require 'evil)
+(evil-mode 1)
+(evil-escape-mode 1)
+(setq-default evil-escape-key-sequence "jk")
+(setq-default evil-escape-delay 0.2)
 
 ;; company mode
 (package-install 'company)
+(setq company-idle-delay 0)
+(setq company-minimum-prefix-length 1)
 (global-company-mode t)
 
-;; mini buffer
-(package-install 'vertico)
-(vertico-mode t)
-(package-install 'orderless)
-(setq completion-styles '(orderless))
-(package-install 'marginalia)
-(marginalia-mode t)
+;; Magit
+(package-install 'magit)
 
-;; ui
-(package-install 'monokai-theme)
-(package-install 'timu-macos-theme)
+;; Flutter
+(package-install 'dart-mode)
+(package-install 'flutter)
 
-;;(load-theme 'monokai 1)
-
-;; Vim Simulation
-;; (package-install 'evil)
-;; (setq evil-want-C-u-scroll t)
-;; (evil-mode t)
-;; (package-install 'evil-escape)
-;; (setq-default evil-escape-key-sequence "jk")
-;; (setq-default evil-escape-delay 0.4)
-;; (evil-escape-mode)
-
-;; (evil-leader/set-key 
-
+;; set custom file
 (setq custom-file (expand-file-name "~/.emacs.d/custom.el"))
