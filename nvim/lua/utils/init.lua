1508307a5923407f8ecd3f530836453a6b6fab4e
Cano XL <gcanoxl@gmail.com>
2025-02-26 12:58:21 +0800

    refactor: create utils.func.lua

--- nvim/lua/utils/init.lua
+++ nvim/lua/utils/init.lua
@@ -1,40 +1,12 @@
 _G.utils = {}
 
-function utils.cmd(cmd)
-	return "<cmd>" .. cmd .. "<cr>"
-end
-
-function utils.map_on_filetype(filetype, maps)
-	vim.api.nvim_create_autocmd("FileType", {
-		pattern = filetype,
-		group = vim.api.nvim_create_augroup("mapping" .. filetype, { clear = true }),
-		callback = function()
-			local buf = vim.api.nvim_get_current_buf()
-			require("which-key").register(maps, { prefix = "<localleader>", buffer = buf })
-		end,
-	})
-end
-
-function utils.code()
-	vim.lsp.buf.code_action({
-		apply = true,
-		filter = function(action)
-			return action.title == "Fix All"
-		end,
-	})
-end
-
-function utils.config_files()
-	local ok, telescope = pcall(require, "telescope.builtin")
-	if not ok then
-		vim.notify("Telescope not found", vim.log.levels.ERROR)
-		return
-	end
-	telescope.find_files({
-		prompt_title = "Config Files",
-		cwd = "~/.config/nvim/",
-	})
-end
+-- TODO: reorganize
+utils.func = require("utils.func")
+setmetatable(utils, {
+	__index = function(_, key)
+		return utils.func[key]
+	end,
+})
 
 utils.toggles = require("utils.toggles")
 utils.bufdelete = require("utils.bufdelete")
