3303ffe380a6974aad3bbdbea30a99e5eb013890
Cano XL <gcanoxl@gmail.com>
2025-03-30 20:23:18 +0800

    chore: use get_clients instead of get_active_clients

--- nvim/lua/configs/heirline/components/ornaments.lua
+++ nvim/lua/configs/heirline/components/ornaments.lua
@@ -6,7 +6,7 @@ local LSPActive = {
 	update = { "LspAttach", "LspDetach" },
 	provider = function()
 		local names = {}
-		for _, server in pairs(vim.lsp.get_active_clients({ bufnr = 0 })) do
+		for _, server in pairs(vim.lsp.get_clients({ bufnr = 0 })) do
 			table.insert(names, server.name)
 		end
 		return "[" .. table.concat(names, " ") .. "]"
