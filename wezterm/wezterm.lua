e4879ed4a1ec77f0a6fc5f8fa933416a2af56296
Cano XL <gcanoxl@gmail.com>
2025-03-30 17:07:16 +0800

    feat(wezterm): disable padding and turn on auto reloading

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,6 +1,8 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
+config.automatically_reload_config = true
+
 config.font = wezterm.font("Operator Mono", {
 	stretch = "Normal",
 	weight = "Book",
@@ -8,5 +10,12 @@ config.font = wezterm.font("Operator Mono", {
 
 config.font_size = 21
 config.line_height = 1.6
+
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
 
 return config
