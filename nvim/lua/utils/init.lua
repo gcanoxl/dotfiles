1e1c83ca1941bee8ed655c71aae7dfe04ee0dc56
Cano XL <gcanoxl@gmail.com>
2025-03-30 17:45:31 +0800

    chore(nvim): delete utils.rename

--- nvim/lua/utils/init.lua
+++ nvim/lua/utils/init.lua
@@ -1,10 +1,8 @@
 _G.utils = {}
 
--- TODO: reorganize
 utils.func = require("utils.func")
 setmetatable(utils, { __index = utils.func })
 
 utils.toggles = require("utils.toggles")
 utils.bufdelete = require("utils.bufdelete")
 utils.term = require("utils.terminal")
-utils.rename = require("utils.rename")
