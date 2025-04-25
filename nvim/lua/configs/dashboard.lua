34a46ebf48889a537af328967db84bbdd50290e8
Cano XL <gcanoxl@gmail.com>
2025-04-25 22:32:33 +0800

    feat(nvim): auto select section

--- nvim/lua/configs/dashboard.lua
+++ nvim/lua/configs/dashboard.lua
@@ -1,13 +1,23 @@
 local dashboard = require("utils.dashboard")
 local is_higherline = vim.api.nvim_win_get_height(0) > 30
-dashboard({
-	sections = {
-		{ section = "header", pane = 1, padding = is_higherline and 2 or 1 },
-		{
-			section = "keys",
-			gap = is_higherline and 1 or 0,
-			padding = is_higherline and 1 or 0,
-		},
-		{ section = "startup" },
+
+local section_highline = {
+	{ section = "header", pane = 1, padding = 2 },
+	{
+		section = "keys",
+		gap = 1,
+		padding = 1,
 	},
+	{ section = "startup" },
+}
+
+local section_lowline = {
+	{ section = "startup", padding = 1 },
+	{
+		section = "keys",
+		gap = 1,
+	},
+}
+dashboard({
+	sections = is_higherline and section_highline or section_lowline,
 })
