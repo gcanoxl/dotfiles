538a5e82d399783fc83d9f88015d5c90b64e5755
Cano XL <gcanoxl@gmail.com>
2025-05-31 13:45:51 +0800

    feat(nvim): add oil.nvim

--- nvim/lua/plugins/filesystem.lua
+++ nvim/lua/plugins/filesystem.lua
@@ -24,4 +24,16 @@ return {
 			require("telescope").load_extension("projects")
 		end,
 	},
+
+	{
+		"stevearc/oil.nvim",
+		---@module 'oil'
+		---@type oil.SetupOpts
+		opts = {},
+		-- Optional dependencies
+		dependencies = { { "echasnovski/mini.icons", opts = {} } },
+		-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+		-- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+		lazy = false,
+	},
 }
