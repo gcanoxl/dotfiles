1de0b1074d05d962a2155c1b3b0cfa28e84d88a7
Cano XL <gcanoxl@gmail.com>
2025-09-24 19:59:18 +0800

    feat(nvim): remove oil.nvim

--- nvim/lua/plugins/swift.lua
+++ nvim/lua/plugins/swift.lua
@@ -5,7 +5,6 @@ return {
 		"MunifTanjim/nui.nvim",
 		-- "folke/snacks.nvim", -- (optional) to show previews
 		-- "nvim-tree/nvim-tree.lua", -- (optional) to manage project files
-		"stevearc/oil.nvim", -- (optional) to manage project files
 		-- "nvim-treesitter/nvim-treesitter", -- (optional) for Quick tests support (required Swift parser)
 	},
 	config = true,
