371b6ff49142e3f9d7987d7315de7ef860064cc2
Cano XL <cano@Canos-MacBook-Air.local>
2025-11-13 04:55:14 +0800

    feat(nvim): add swift preview refresh mechanism

--- nvim/ftplugin/swift.lua
+++ nvim/ftplugin/swift.lua
@@ -1,13 +1,15 @@
-local swift_group = vim.api.nvim_create_augroup("swift_group", { clear = true })
+local fullpath = "/tmp/xcodebuild.nvim/"
+_G.handle = _G.handle or vim.uv.new_fs_event()
 
-vim.api.nvim_create_autocmd("BufWritePost", {
-	group = swift_group,
-	pattern = "*.swift",
-	callback = function()
-		vim.defer_fn(function()
-			vim.schedule(function()
-				vim.cmd.checktime()
-			end)
-		end, 1000)
-	end,
-})
+if _G.handle then
+	_G.handle:start(fullpath, {
+		recursive = false,
+		stat = true,
+		watch_entry = true,
+		persistent = true,
+	}, function(_, _, _)
+		vim.schedule(function()
+			vim.cmd.checktime()
+		end)
+	end)
+end
