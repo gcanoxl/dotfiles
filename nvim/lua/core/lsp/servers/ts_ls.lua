2d88d0f2976ea886993fc5d650efc728df3506a8
Cano XL <gcanoxl@gmail.com>
2025-12-18 13:10:36 +0800

    fix: add root_markers for ts_ls

--- nvim/lua/core/lsp/servers/ts_ls.lua
+++ nvim/lua/core/lsp/servers/ts_ls.lua
@@ -1,6 +1,7 @@
 return {
 	init_options = { hostInfo = "neovim" },
 	cmd = { "typescript-language-server", "--stdio" },
+	root_markers = vim.list_extend(core.configs.root_markers, { "pacakage.json" }),
 	filetypes = {
 		"javascript",
 		"javascriptreact",
