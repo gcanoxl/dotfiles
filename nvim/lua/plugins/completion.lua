629d5bfb0bef776c08bbcc23d309bb1dcf49b451
Cano XL <gcanoxl@gmail.com>
2026-02-21 09:02:38 +0800

    fix(nvim): nest blink.cmp term config under cmdline

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -82,29 +82,29 @@ return {
 					ghost_text = { enabled = true },
 				},
 			},
-		},
-		term = {
-			enabled = false,
-			keymap = { preset = "inherit" }, -- Inherits from top level `keymap` config when not set
-			sources = {},
-			completion = {
-				trigger = {
-					show_on_blocked_trigger_characters = {},
-					show_on_x_blocked_trigger_characters = nil, -- Inherits from top level `completion.trigger.show_on_blocked_trigger_characters` config when not set
-				},
-				-- Inherits from top level config options when not set
-				list = {
-					selection = {
-						-- When `true`, will automatically select the first item in the completion list
-						preselect = nil,
-						-- When `true`, inserts the completion item automatically when selecting it
-						auto_insert = nil,
+			term = {
+				enabled = false,
+				keymap = { preset = "inherit" }, -- Inherits from top level `keymap` config when not set
+				sources = {},
+				completion = {
+					trigger = {
+						show_on_blocked_trigger_characters = {},
+						show_on_x_blocked_trigger_characters = nil, -- Inherits from top level `completion.trigger.show_on_blocked_trigger_characters` config when not set
 					},
+					-- Inherits from top level config options when not set
+					list = {
+						selection = {
+							-- When `true`, will automatically select the first item in the completion list
+							preselect = nil,
+							-- When `true`, inserts the completion item automatically when selecting it
+							auto_insert = nil,
+						},
+					},
+					-- Whether to automatically show the window when new completion items are available
+					menu = { auto_show = nil },
+					-- Displays a preview of the selected item on the current line
+					ghost_text = { enabled = nil },
 				},
-				-- Whether to automatically show the window when new completion items are available
-				menu = { auto_show = nil },
-				-- Displays a preview of the selected item on the current line
-				ghost_text = { enabled = nil },
 			},
 		},
 		opts_extend = { "sources.default" },
