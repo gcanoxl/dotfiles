276f72bb7df8a845b5abdc59a215fe21d0a5d032
Cano XL <gcanoxl@gmail.com>
2026-07-03 16:29:00 +0800

    feat(nvim): add sourcekit support

---
+++ nvim/lua/lsp/configs/sourcekit.lua
@@ -0,0 +1,30 @@
+---@brief
+---
+--- https://github.com/swiftlang/sourcekit-lsp
+---
+--- Language server for Swift and C/C++/Objective-C.
+
+---@type vim.lsp.Config
+return {
+  cmd = { 'sourcekit-lsp' },
+  mason = false,
+  filetypes = { 'swift', 'objc', 'objcpp' },
+  root_markers = { '.project', '.git' },
+  get_language_id = function(_, ftype)
+    local t = { objc = 'objective-c', objcpp = 'objective-cpp' }
+    return t[ftype] or ftype
+  end,
+  capabilities = {
+    workspace = {
+      didChangeWatchedFiles = {
+        dynamicRegistration = true,
+      },
+    },
+    textDocument = {
+      diagnostic = {
+        dynamicRegistration = true,
+        relatedDocumentSupport = true,
+      },
+    },
+  },
+}
